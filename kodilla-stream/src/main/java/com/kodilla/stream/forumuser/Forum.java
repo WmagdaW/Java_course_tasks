4b0af694df60595a814249118234972095e35671
Magdalena Wisniewska <magda.zz@hotmail.com>
2021-10-27 09:39:06 +0200

    7.3 Funkcyjny spacer po liście użytkowników forum

--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -7,12 +7,12 @@ public class Forum {
     private final List<ForumUser> userList = new ArrayList<>();
 
     public List<ForumUser> getUserList(){
-    userList.add( new ForumUser(001, "Mike", 'M', 1980,01,01, 0 ));
-    userList.add( new ForumUser(002, "John", 'M', 2010, 02, 01, 10 ));
-    userList.add( new ForumUser(003, "Tom", 'M', 1982,03,01, 15 ));
-    userList.add( new ForumUser(004, "Chris", 'M', 2001,10,30, 15 ));
-    userList.add( new ForumUser(005, "Kate", 'F', 1983,04,01, 20 ));
-    userList.add( new ForumUser(006, "Alex", 'M', 2001,10,20, 5 ));
+    userList.add( new ForumUser(1, "Mike", 'M', 1980,1,1, 0 ));
+    userList.add( new ForumUser(2, "John", 'M', 2010, 2, 1, 10 ));
+    userList.add( new ForumUser(3, "Tom", 'M', 1982,3,1, 15 ));
+    userList.add( new ForumUser(4, "Chris", 'M', 2001,10,30, 15 ));
+    userList.add( new ForumUser(5, "Kate", 'F', 1983,4,1, 20 ));
+    userList.add( new ForumUser(6, "Alex", 'M', 2001,10,20, 5 ));
     return new ArrayList<>(userList);
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/StreamMain.java
@@ -11,7 +11,7 @@ public class StreamMain {
         Forum theForum = new Forum();
         Map <Integer, ForumUser> theResultMapOfForumUsers= theForum.getUserList().stream()
                 .filter(forumUser -> forumUser.getSex() == 'M')
-                .filter (forumUser ->((LocalDate.now().getYear())-(forumUser.getDateOfBirth().getYear())>20)||((LocalDate.now().getYear())-(forumUser.getDateOfBirth().getYear())==20)&&(forumUser.getDateOfBirth().getMonthValue())<(LocalDate.now().getMonthValue())||((LocalDate.now().getYear())-(forumUser.getDateOfBirth().getYear())==20)&&((forumUser.getDateOfBirth().getMonthValue())==(LocalDate.now().getMonthValue()))&&((forumUser.getDateOfBirth().getDayOfMonth())<= (LocalDate.now().getDayOfMonth())))
+                .filter (forumUser -> Period.between(forumUser.getDateOfBirth(), LocalDate.now()).getYears()>=20)
                 .filter (forumUser -> forumUser.getPostNo() > 0)
                 .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
 
