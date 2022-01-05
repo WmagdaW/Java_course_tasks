89b6de72b7afb0ebd6ce2da96f767b1e47b51f68
Magdalena Wisniewska <magda.zz@hotmail.com>
2022-01-05 13:23:47 +0100

    Revert "13.1 Funkcyjna iteracja po tytułach"
    This reverts commit fbb5c40ba543dd34e16aa952516a06c54194b94c.

--- kodilla-good-patterns/src/main/java/Food2Door/HealthyShop.java
+++
@@ -1,8 +0,0 @@
-package Food2Door;
-
-public class HealthyShop implements OrderProcessor {
-
-    public void process() {
-        System.out.println("Please contact us for extra discount!");
-    }
-}
