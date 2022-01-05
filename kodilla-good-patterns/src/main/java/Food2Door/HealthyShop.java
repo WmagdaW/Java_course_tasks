fbb5c40ba543dd34e16aa952516a06c54194b94c
Magdalena Wisniewska <magda.zz@hotmail.com>
2022-01-05 13:10:20 +0100

    13.1 Funkcyjna iteracja po tytułach

---
+++ kodilla-good-patterns/src/main/java/Food2Door/HealthyShop.java
@@ -0,0 +1,8 @@
+package Food2Door;
+
+public class HealthyShop implements OrderProcessor {
+
+    public void process() {
+        System.out.println("Please contact us for extra discount!");
+    }
+}
