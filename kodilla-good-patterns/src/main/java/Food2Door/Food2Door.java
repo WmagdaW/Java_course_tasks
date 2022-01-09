db428d359b792efd3d34d9640d84dc2a41c32ebe
Magdalena Wisniewska <magda.zz@hotmail.com>
2022-01-09 21:35:26 +0100

    13.4 Food2Door

--- kodilla-good-patterns/src/main/java/Food2Door/Food2Door.java
+++ kodilla-good-patterns/src/main/java/Food2Door/Food2Door.java
@@ -1,6 +1,5 @@
 package Food2Door;
 
-
 public class Food2Door {
     public static void main(String[] args) {
         OrderProcessor extraFoodShop = new ExtraFoodShop();
@@ -10,12 +9,16 @@ public class Food2Door {
         OrderMaker orderMaker2 = new OrderMaker(glutenFreeShop);
         OrderMaker orderMaker3 = new OrderMaker(healthyShop);
 
-        orderMaker3.makeOrder();
-
         OrderRetriever orderRequestRetriever = new OrderRetriever();
         OrderRequest orderRequest = orderRequestRetriever.retrieve();
 
         OrderServiceProcessor orderServiceProcessor = new OrderServiceProcessor(new FoodOrderService(), new FoodOrderRepository());
+
+        orderMaker1.makeOrder();
         orderServiceProcessor.processOrder(orderRequest);
+
+        orderMaker2.makeOrder();
+        orderMaker3.makeOrder();
+
     }
 }
---
+++ kodilla-good-patterns/src/main/java/Food2Door/HealthyShop.java
@@ -0,0 +1,10 @@
+package Food2Door;
+
+public class HealthyShop implements OrderProcessor {
+
+    public void process() {
+
+        System.out.println("Contact us to get an extra discount!");
+    }
+}
+
