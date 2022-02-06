a09281d33fa11f968a4fe1b096b6b3bb5f637b6d
Magdalena Wisniewska <magda.zz@hotmail.com>
2022-02-06 14:05:36 +0100

    15.5 Builder

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,27 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BigmacTestSuite {
+
+    @Test
+    void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun ("no sesame")
+                .burgers (1)
+                .sauce ("standard")
+                .ingredient ("cheese")
+                .ingredient ("onion")
+                .ingredient ("bacon")
+                .ingredient ("mushrooms")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        assertEquals(4, howManyIngredients);
+    }
+}
