03c812a8ef39c51ed62f013e0e003f1f8f23271b
Magdalena Wisniewska <magda.zz@hotmail.com>
2022-02-16 09:38:28 +0100

    17.2  Tworzymy encję list zadań

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class TaskListDaoTestSuite {
@@ -31,9 +32,43 @@ public class TaskListDaoTestSuite {
 
         //Then
         assertEquals(1, theList.size());
+        assertNotNull(theList.get(0).getId());
+        assertEquals(LISTNAME, theList.get(0).getListName());
+        assertEquals(DESCRIPTION, theList.get(0).getDescription());
 
         //CleanUp
         int id = theList.get(0).getId();
         taskListDao.deleteById(id);
+    }
+
+    @Test
+    void testFindByListNameThatDoesntExist() {
+        //Given
+
+        //When
+        List<TaskList> list2 = taskListDao.findByListName("list2");
+
+        //Then
+        assertEquals(0, list2.size());
+    }
+
+    @Test
+    void testFindByListNameWhenNameIsDuplicated() {
+        //Given
+        TaskList taskList = new TaskList(ID, LISTNAME, DESCRIPTION);
+        TaskList taskList1 = new TaskList(ID, LISTNAME, "description1");
+        TaskList taskList2 = new TaskList(ID, "New list", "description2");
+        taskListDao.save(taskList);
+        taskListDao.save(taskList1);
+        taskListDao.save(taskList2);
+
+        //When
+        List<TaskList> list3 = taskListDao.findByListName(LISTNAME);
+
+        //Then
+        assertEquals(2, list3.size());
+
+        //CleanUp
+        taskListDao.deleteAll();
     }
 }
